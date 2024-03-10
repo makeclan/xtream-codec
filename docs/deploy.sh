756e0b1b8c15692cf3c8686d8a218a5351003db6
hylexus <hylexus@163.com>
2024-03-10 18:45:53 +0800

    docs(docs): ...

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -13,7 +13,7 @@ git add -A
 git commit -m 'deploy docs'
 #
 ## 如果发布到 https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:hylexus/jt-framework.git main:gh-pages
+git push -f git@github.com:hylexus/xtream-codec.git main:gh-pages
 git push -f git@gitee.com:hylexus/xtream-codec.git main:gh-pages
 #
 cd -
