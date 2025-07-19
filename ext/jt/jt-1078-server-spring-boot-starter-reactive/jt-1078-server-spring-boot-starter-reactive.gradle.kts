f900081ca05f26d40993dfc9004a46b601f85416
hylexus <hylexus@163.com>
2025-07-19 20:08:54 +0800

    build[gradle]: upgrade to gradle-8.14.3

--- ext/jt/jt-1078-server-spring-boot-starter-reactive/jt-1078-server-spring-boot-starter-reactive.gradle.kts
+++ ext/jt/jt-1078-server-spring-boot-starter-reactive/jt-1078-server-spring-boot-starter-reactive.gradle.kts
@@ -1,3 +1,5 @@
+import org.cadixdev.gradle.licenser.LicenseExtension
+
 dependencies {
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -23,4 +25,12 @@ dependencies {
     implementation("jakarta.annotation:jakarta.annotation-api")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+val jt1078MockFileDir = project.file("src/test/resources/mock-data")
+// 不检测 License
+extensions.configure(LicenseExtension::class.java) {
+    exclude {
+        it.file.startsWith(jt1078MockFileDir)
+    }
 }
